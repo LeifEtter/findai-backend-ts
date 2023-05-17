7373d59a239bc9aea7df1e0b8ae870b900aa4d92
Leif Etter <leif.etter@gmx.de>
2023-05-17 22:57:14 +0200

    some testing configuration

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,9 @@ import type { Config } from "jest";
 
 const config: Config = {
   verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFiles: ["dotenv/config"],
 };
 
 export default config;
