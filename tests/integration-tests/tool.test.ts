9eab748ab2af2ddf89eea121eff4ed3cd1863857
Leif Etter <leif.etter@gmx.de>
2023-05-17 22:56:52 +0200

    initial test folder/file structure

---
+++ tests/integration-tests/tool.test.ts
@@ -0,0 +1,9 @@
+// import * as express from "express";
+// import * as supertest from "supertest";
+// import
+
+// describe("Testing of the tool routes", () => {
+//   it("should get a single tool by id", async () => {
+//     await request
+//   });
+// });
