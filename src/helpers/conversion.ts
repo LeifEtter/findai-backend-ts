65c96ad183a255938ab757d70af69962927dd0ae
Leif Etter <leif.etter@gmx.de>
2023-05-18 17:26:31 +0200

    add file with conversions of query strings as helper

---
+++ src/helpers/conversion.ts
@@ -0,0 +1,44 @@
+import QueryString from "qs";
+
+const convertQueryToList = (
+  query:
+    | string
+    | string[]
+    | QueryString.ParsedQs
+    | QueryString.ParsedQs[]
+    | undefined
+): string[] => {
+  let result: string[] = [];
+  if (!query) {
+    result = [];
+  } else if (!Array.isArray(query)) {
+    result = [query as string];
+  } else {
+    result = query as Array<string>;
+  }
+  return result;
+};
+
+const convertOrderQueryToOrderObject = (
+  orderBy: string | undefined
+): object => {
+  if (!orderBy) {
+    return {};
+  } else if (orderBy == "bookmarks") {
+    return {
+      bookmarkedBy: {
+        _count: "desc",
+      },
+    };
+  } else if (orderBy == "upvotes") {
+    return {
+      upvotes: {
+        _count: "desc",
+      },
+    };
+  } else {
+    return {};
+  }
+};
+
+export { convertQueryToList, convertOrderQueryToOrderObject };
