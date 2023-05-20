38334f4b56df0c3ea7f9765ddf865cd716177eb4
Leif Etter <leif.etter@gmx.de>
2023-05-20 02:16:59 +0200

    add authentication middleware to check token validity and extract user id and assign it to body object of request

--- src/helpers/authentication.ts
+++ src/helpers/authentication.ts
@@ -1,23 +1,35 @@
 import jwt from "jsonwebtoken";
+import { JwtPayload } from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import * as dotenv from "dotenv";
+
 dotenv.config();
 
 const extractToken = (req: Request, res: Response, next: NextFunction) => {
   if (!req.headers.authorization) {
     return res.status(403).send({ message: "Please provide a token" });
   }
-  const jwtToken: string = req.headers.authorization.split("")[1];
+  let jwtToken: string;
+  if (req.headers.authorization.startsWith("Bearer" || "bearer")) {
+    jwtToken = req.headers.authorization.split(" ")[1];
+  } else {
+    jwtToken = req.headers.authorization;
+  }
   try {
-    const verifiedToken = jwt.verify(jwtToken, process.env.JWT_SECRET || "");
-    console.log(verifiedToken);
+    const verifiedToken: JwtPayload | string = jwt.verify(
+      jwtToken,
+      process.env.JWT_SECRET!
+    );
+    if (typeof verifiedToken != "string") {
+      req.body.userId = verifiedToken.userId;
+    }
+    next();
   } catch (error) {
     return res.status(403).send({
       message:
         "Your token can't be verified, the time of validity might have passed",
     });
   }
-  next();
 };
 
 export default extractToken;
