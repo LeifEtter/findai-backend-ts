a28b2bae5773ec5de1f06f315c6c28a5cea13feb
Leif Etter <leif.etter@gmx.de>
2023-05-21 15:32:31 +0200

    changed validation to val

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -8,12 +8,12 @@ import {
 } from "../controllers/user-controller";
 import checkValid from "../helpers/check-valid";
 import extractToken from "../helpers/authentication";
-import validation from "../helpers/validation";
+import val from "../helpers/validation";
 
 const userRouter: express.Router = express.Router();
 
 userRouter.route("/profile").get(extractToken, getUserById, getUserProfile);
-userRouter.route("/login").get(checkValid(validation.login), login);
-userRouter.route("/register").post(checkValid(validation.register), register);
-userRouter.route("/verify").patch(checkValid(validation.verify), verify);
+userRouter.route("/login").get(checkValid(val.login), login);
+userRouter.route("/register").post(checkValid(val.register), register);
+userRouter.route("/verify").patch(checkValid(val.verify), verify);
 export default userRouter;
