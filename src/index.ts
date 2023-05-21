397960227ec618852e513a6fa0887215862df93a
Leif Etter <leif.etter@gmx.de>
2023-05-21 16:27:27 +0200

    add checks for .env file airtable tool and tag id's

--- src/index.ts
+++ src/index.ts
@@ -37,9 +37,14 @@ if (!process.env.NODE_ENV) {
   console.error(
     "Please add 'SENDGRID_API_KEY=[api key from sendgrid]' to allow the sending of emails"
   );
-} else if (!process.env.AIRTABLE_URL || !process.env.AIRTABLE_TOKEN) {
+} else if (
+  !process.env.AIRTABLE_URL ||
+  !process.env.AIRTABLE_TOKEN ||
+  !process.env.AIRTABLE_TOOL_TABLE_ID ||
+  !process.env.AIRTABLE_TAG_TABLE_ID
+) {
   console.error(
-    "Please add 'AIRTABLE_URL=[url]' and 'AIRTABLE_TOKEN=[token]' to the env file"
+    "Please add 'AIRTABLE_URL=[url]' and 'AIRTABLE_TOKEN=[token]' to the env file, also add 'AIRTABLE_TOOL_TABLE_ID=[id]' and 'AIRTABLE_TAG_TABLE_ID=[id]'"
   );
 } else if (!process.env.PORT) {
   console.error("Please add 'PORT=[port to run server on]' to the .env file");
