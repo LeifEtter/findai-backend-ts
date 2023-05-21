fb2d77d86dd58d18c6d9ce99520f6ce04597da51
Leif Etter <leif.etter@gmx.de>
2023-05-21 17:19:18 +0200

    add tag router to main router file

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import toolRouter from "./routes/tool";
 import userRouter from "./routes/user";
+import tagRouter from "./routes/tag";
 
 const api = express.Router();
 
 api.use("/tools", toolRouter);
 api.use("/user", userRouter);
+api.use("/tag", tagRouter);
 
 export default api;
