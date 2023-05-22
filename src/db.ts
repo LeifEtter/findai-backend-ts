32302eacddb9fc767aeb8f770f592ad17088b2c5
Leif Etter <etter.leif@gmail.com>
2023-05-22 16:05:10 +0200

    comment out log levels to prevent logging; fix later;

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma: PrismaClient = new PrismaClient({
-  log: ["query", "info", "warn", "error"],
-});
+// const prisma: PrismaClient = new PrismaClient({
+//   log: ["query", "info", "warn", "error"],
+// });
+
+const prisma: PrismaClient = new PrismaClient();
 
 export default prisma;
